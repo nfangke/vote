8de8cb0027d856b708bc9dd17600379d2f53a566
Nancy Fang and Ryan Carlile <ryan@>
2016-03-24 15:09:46 -0700

    add politics site to repository

---
+++ js/politics.js
@@ -0,0 +1,26 @@
+var add = function(question1, question2, question3, question4, question5) {
+  return question1 + question2 + question3 + question4 +question5
+}
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+    var question1 = parseInt($("#question1").val());
+    var question2 = parseInt($("#question2").val());
+    var question3 = parseInt($("#question3").val());
+    var question4 = parseInt($("#question4").val());
+    var question5 = parseInt($("#question5").val());
+    var result = add(question1, question2, question3, question4, question5);
+
+    if (result <50) {
+      $(".resultA").show();
+    } else if ((result <100) && (result >50)) {
+      $(".resultB").show();
+    }  else if ((result <140) && (result >=100)){
+        $(".resultC").show();
+      }  else { $(".resultD").show(); }
+
+
+  });
+});
